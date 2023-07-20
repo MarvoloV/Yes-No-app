415a2b43a7a5ab5861b2354c420cced7dd4fc6c2
Jorge Luis Adco Ccahuay <jorgeluis@Jorges-Mac-Pro.local>
2023-07-20 00:53:08 -0500

    add ListView

--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -6,15 +6,38 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: const Padding(
-          padding: EdgeInsets.all(4.0),
-          child: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  "https://i.pinimg.com/564x/c1/d2/9f/c1d29f36d6891c0231e0195f1ce74163.jpg")),
+        appBar: AppBar(
+          leading: const Padding(
+            padding: EdgeInsets.all(4.0),
+            child: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://i.pinimg.com/564x/c1/d2/9f/c1d29f36d6891c0231e0195f1ce74163.jpg")),
+          ),
+          title: const Text('Mi Amor'),
+          centerTitle: false,
         ),
-        title: const Text('Mi Amor'),
-        centerTitle: false,
+        body: _ChatView());
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return Text("indice: $index");
+              },
+            )),
+            Text("Mundo")
+          ],
+        ),
       ),
     );
   }
