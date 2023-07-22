1055a81f075f3fd9db7eaf35848207614938bcae
Jorge luis Adco Ccahuay <jorgead0812@gmail.com>
2023-07-21 23:19:23 -0500

    add scrolltoBottom

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -2,14 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final ScrollController chatScrollController = ScrollController();
   List<Message> messageList = [
     Message(text: 'Holis mi tubi mor', fromWho: FromWho.me),
     Message(text: "Ya regresaste del Trabajo?", fromWho: FromWho.me)
   ];
   Future<void> sendMessage(String text) async {
-    //TODO implementar método
+    if (text.isEmpty) return;
     final newMessage = Message(text: text, fromWho: FromWho.me);
     messageList.add(newMessage);
+    modeScrollToBotton();
     notifyListeners();
+  }
+
+  void modeScrollToBotton() {
+    chatScrollController.animateTo(
+        chatScrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOut);
   }
 }
